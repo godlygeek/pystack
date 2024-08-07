2b807305ac1dec32cc900cb105f236f5a2c7e135
Matt Wozniski <mwozniski@bloomberg.net>
2024-08-07 18:55:23 -0400

    wip

---
+++ src/pystack/_pystack/cpython/lock.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <cstdint>
+
+namespace pystack {
+
+namespace Python3_13 {
+struct PyMutex
+{
+    uint8_t v;
+};
+}  // namespace Python3_13
+
+namespace Python3_13t {
+struct PyMutex
+{
+    uint8_t v;
+};
+}  // namespace Python3_13t
+
+}  // namespace pystack
